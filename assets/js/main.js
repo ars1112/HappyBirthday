75c0b6fcae566adbcda8148a44a869489f6462e7
ars1112 <159202112+ars1112@users.noreply.github.com>
2024-02-15 10:44:00 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// script.js
+function showModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
